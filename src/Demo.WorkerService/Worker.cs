037cef9d1e8b1a7bc7251044a35586a58c23cd3d
Ekkapong <gold_exp@hotmail.com>
2024-02-23 19:35:45 +0700

    feat : read message from google

--- src/Demo.WorkerService/Worker.cs
+++ src/Demo.WorkerService/Worker.cs
@@ -1,24 +1,43 @@
+using Demo.WorkerService.Options;
+using Google.Cloud.PubSub.V1;
+using Microsoft.Extensions.Options;
+
 namespace Demo.WorkerService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IOptions<PubSubOption> _options;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IOptions<PubSubOption> options)
         {
             _logger = logger;
+            _options = options;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            // Pull messages from the subscription using SubscriberClient.
+            SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(_options.Value.ProjcetId, _options.Value.SubscriptionId);
+            SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+
+            // Start the subscriber listening for messages.
+            await subscriber.StartAsync((msg, cancellationToken) =>
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+
+                Console.WriteLine($"Received message {msg.MessageId} published at {msg.PublishTime.ToDateTime()}");
+                Console.WriteLine($"Text: '{msg.Data.ToStringUtf8()}'");
+
+                foreach (var attr in msg.Attributes.ToDictionary())
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    Console.WriteLine($"Attribute: '{attr.Key}'");
+                    Console.WriteLine($"Value: '{attr.Value}'");
                 }
-                await Task.Delay(1000, stoppingToken);
-            }
+
+                return Task.FromResult(SubscriberClient.Reply.Ack);
+            });
+
+            await subscriber.StopAsync(stoppingToken);
         }
     }
 }
